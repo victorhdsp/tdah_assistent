4d5a4de64411bb26c791328f4c7c66cc093265ca
victorhdsp <victorhugods.pereira@gmail.com>
2025-07-08 14:51:27 -0300

    refactor: :sparkles: get all relevant information in node

--- android/app/src/main/java/com/anonymous/tdah_assistent/EntryAccessibilityService.kt
+++ android/app/src/main/java/com/anonymous/tdah_assistent/EntryAccessibilityService.kt
@@ -2,47 +2,98 @@ package com.anonymous.tdah_assistent
 
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
-import com.facebook.react.modules.core.DeviceEventManagerModule
-import com.facebook.react.ReactApplication
+import android.view.accessibility.AccessibilityNodeInfo
+import android.util.Log
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.Arguments
 
-
-fun mapAccessibilityEventToWritableMap(event: AccessibilityEvent): WritableMap {
+fun mapNodeToWritableMap(node: AccessibilityNodeInfo): WritableMap {
     return Arguments.createMap().apply {
-        putInt("eventType", event.eventType)
-        putString("eventText", event.text?.toString())
-        putString("packageName", event.packageName?.toString())
-        putString("className", event.className?.toString())
-        putInt("itemCount", event.itemCount)
-        putInt("currentItemIndex", event.currentItemIndex)
-        putInt("fromIndex", event.fromIndex)
-        putInt("toIndex", event.toIndex)
-        putInt("scrollX", event.scrollX)
-        putInt("scrollY", event.scrollY)
-        putBoolean("isChecked", event.isChecked)
-        putBoolean("isEnabled", event.isEnabled)
-        putBoolean("isPassword", event.isPassword)
+        putString("viewIdResourceName", node.viewIdResourceName)
+        putString("className", node.className?.toString())
+        putString("text", node.text?.toString())
+        putString("contentDescription", node.contentDescription?.toString())
+        putBoolean("isClickable", node.isClickable)
+        putBoolean("isEnabled", node.isEnabled)
+        putBoolean("isFocusable", node.isFocusable)
+        putBoolean("isFocused", node.isFocused)
+        putBoolean("isVisibleToUser", node.isVisibleToUser)
+        putBoolean("isAccessibilityFocused", node.isAccessibilityFocused)
+        putBoolean("isSelected", node.isSelected)
+        putBoolean("isCheckable", node.isCheckable)
+        putBoolean("isChecked", node.isChecked)
+        putBoolean("isScrollable", node.isScrollable)
+        putBoolean("isLongClickable", node.isLongClickable)
+        putBoolean("isPassword", node.isPassword)
+        putBoolean("isEditable", node.isEditable)
+        putBoolean("isDismissable", node.isDismissable)
+        putBoolean("isImportantForAccessibility", node.isImportantForAccessibility)
+        putInt("inputType", node.inputType)
+        putInt("liveRegion", node.liveRegion)
+        putInt("movementGranularities", node.movementGranularities)
+        putInt("childCount", node.childCount)
+
+        // Bounds do elemento na tela (útil para análise espacial)
+        val rect = android.graphics.Rect()
+        node.getBoundsInScreen(rect)
+        val bounds = Arguments.createMap().apply {
+            putInt("left", rect.left)
+            putInt("top", rect.top)
+            putInt("right", rect.right)
+            putInt("bottom", rect.bottom)
+        }
+        putMap("boundsInScreen", bounds)
     }
+}
+
+fun mapNodeWithChildren(node: AccessibilityNodeInfo): WritableMap {
+    val map = mapNodeToWritableMap(node)
+    val childrenArray = Arguments.createArray()
+
+    for (i in 0 until node.childCount) {
+        val childNode = node.getChild(i)
+        if (childNode != null) {
+            childrenArray.pushMap(mapNodeWithChildren(childNode))
+            childNode.recycle() // ⚠️ Liberar memória
+        }
+    }
+
+    map.putArray("children", childrenArray)
+    return map
 }
 
 class EntryAccessibilityService : AccessibilityService() {
     override fun onServiceConnected() {
         super.onServiceConnected()
-        android.util.Log.d("ACCESS_SERVICE", "Serviço iniciado!")
+        Log.d("ACCESS_SERVICE", "Serviço de acessibilidade iniciado")
     }
-    
+
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        if (event != null) {
-            android.util.Log.d("ACCESS_SERVICE", "Evento de acessibilidade recebido: ${event?.eventType}")
-            val params = mapAccessibilityEventToWritableMap(event)
-            if (EntryModule.instance != null) {
-                EntryModule.instance!!.emitAccessibilityEvent(params)
-            } else {
-                android.util.Log.w("ACCESS_SERVICE", "Módulo EntryModule ainda não está pronto")
+        if (event == null || EntryModule.instance == null) return
+
+        Log.d("ACCESS_SERVICE", "Evento recebido: ${event.eventType}")
+
+        val node: AccessibilityNodeInfo? = rootInActiveWindow ?: event.source
+        val appPackage = event.packageName?.toString() ?: "Desconhecido"
+
+        if (node != null) {
+            try {
+                val serialized = mapNodeWithChildren(node)
+                serialized.putString("eventType", AccessibilityEvent.eventTypeToString(event.eventType))
+                serialized.putString("packageName", appPackage)
+                EntryModule.instance?.emitAccessibilityEvent(serialized)
+                Log.d("ACCESS_SERVICE", "Evento emitido para JS com árvore")
+            } catch (e: Exception) {
+                Log.e("ACCESS_SERVICE", "Erro ao mapear árvore de acessibilidade", e)
+            } finally {
+                node.recycle() // ⚠️ SEMPRE libere!
             }
+        } else {
+            Log.w("ACCESS_SERVICE", "Nenhum node disponível (root e source são null)")
         }
     }
 
-    override fun onInterrupt() {}
+    override fun onInterrupt() {
+        Log.w("ACCESS_SERVICE", "Serviço de acessibilidade interrompido")
+    }
 }
